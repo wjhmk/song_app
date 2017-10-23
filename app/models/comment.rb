9e623a2785a35e69057ee5e660e977b9669f5462
Ren <your.email@example.com>
2017-10-23 04:30:41 +0000

    ssss

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,19 +3,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :song
   
-  has_attached_file :image,
-                    :styles => {
-                        :thumb  => "180x180",
-                        :medium => "400x400",
-                        :large => "600x400"
-                    },
-                    :default_url => "noimage.jpg",
+  has_attached_file :image, styles: { :medium => "400x400#", :thumb => "180x180#" }, :default_url => "noimage.jpg",
                     :storage => :s3,
-                    :s3_permissions => :private,
                     :s3_credentials => "#{Rails.root}/config/s3.yml",
                     :path => ":attachment/:id/:style.:extension"
  
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+ 
  
   def authenticated_image_url(style)
     image.s3_object(style).url_for(:read, :secure => true)
